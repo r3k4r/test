5a9bc11fa40575c56a86092e3940626b15cdd9b0
r3k4r <rekarjamal33@gmail.com>
2024-09-26 00:34:55 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    test
+      <div className="w-full flex items-center justify-center">
+          <div className="flex items-center justify-center w-full h-svh bg-[#36a5c1] ">
+            <div className="flex flex-col items-center justify-centers gap-8">
+              <h1 className="text-4xl font-semibold text-white">Uzmanlıklara göre göz atın</h1>
+              <p className="text-white text-md font-normal w-[70%] text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+
+            <div>
+              
+            </div>
+
+
+          </div>
+      </div>
     </>
   );
 }
